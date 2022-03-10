cd2a2529b5b97ff44116454766a0b0b13f4aa5b5
Matt Braden <mjbraden@umich.edu>
2022-03-10 14:31:36 -0500

    added real code

--- AddIntegers.java
+++ AddIntegers.java
@@ -1,4 +1,13 @@
-public static void main(stirng[] args){
-  int a = 1;
-  int b = 2;
+public class AddIntegers {
+
+    public static void main(String[] args) {
+
+        int first = 15;
+        int second = 25;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
 }
